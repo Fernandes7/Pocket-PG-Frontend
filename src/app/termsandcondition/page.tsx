2f07e411085ffc164a22d2f756b6b22e3dda2979
minna <elizabethminna18@gmail.com>
2024-03-13 21:14:24 +0530

    finish

--- src/app/termsandcondition/page.tsx
+++ src/app/termsandcondition/page.tsx
@@ -1,8 +1,123 @@
+import styles from "./terms.module.css"
 export default function TermsandCondition()
 {
     return(
-        <>
-        Terms and Condition page
-        </>
+        <div>
+        <div className={styles.fig}>
+        <h1 className={styles.heading}>Terms and Conditions</h1>
+        </div>
+        <div>
+           <h2 className={styles.heading2}>Introduction</h2>
+           <p className={styles.p1}>
+            These are the Terms and Conditions that govern your use of the PocketPG website ("www.pocketpg.in"). By accessing and using this website, you agree to   
+            comply with these Terms and Conditions. Please review this page carefully. If you do not agree with any part of these Terms and Conditions, we kindly 
+            ask you to refrain from using this website. PocketPG and its associated entities have the right to update or modify these Terms and Conditions at any 
+            time without prior notice, so we recommend checking this page periodically for any changes.</p>
+        </div>
+
+        <div>
+            <h2 className={styles.heading3}>Use of Content</h2>
+            <p className={styles.p1}>
+            All logos, trademarks, brands, names, and other content displayed on this website are the property of FindMyHostel or its associated entities, unless 
+            otherwise stated. You are strictly prohibited from selling, modifying, reproducing, publicly performing, distributing, or using the content on this 
+            website for any commercial or public purpose without written permission from the respective organization or entity.
+            </p>
+        </div>
+
+        <div>
+            <h2 className={styles.heading3}>Acceptable Website Use</h2>
+            <p className={styles.p1}>
+                <b>(A) Security Rules:</b> <br />
+                Unauthorized attempts to access data or servers, probe vulnerabilities, interfere with services, or send unsolicited electronic mail are strictly prohibited. 
+                Violations may result in civil or criminal liability, and the business reserves the right to investigate and cooperate with law enforcement authorities.
+                <br /><b>(B) General Rules: <br /></b>
+                Users are prohibited from transmitting, distributing, storing, or destroying material that could be considered a criminal offense, infringes intellectual property rights, or is defamatory, obscene, or hateful.
+                <br /><b>(C)Privacy Policy:</b>
+                By using this website, you acknowledge and consent to the Privacy Policy. If you do not agree, please refrain from using the website.
+                <br /><b>(D)Indemnity:</b>
+                You agree to indemnify and hold PocketPG, its officers, directors, employees, and agents harmless from any claims or damages arising from your use of the website or violation of these terms.
+                <br /><b>(E)Liability:</b>
+                PocketPG and its group companies, directors, officers, or employees shall not be liable for any direct, indirect, incidental, special, or consequential damages resulting from your use of the website.
+            </p>
+        </div>
+
+        <div>
+            <h2 className={styles.heading3}>OUR ROLE:</h2>
+            <p className={styles.p1}>
+            PocketPG provides registered users (referred to as "User") with the option to make rental payments through its platform, which includes the website and mobile app. 
+            These payments are processed using a third-party payment gateway. By using this platform for rental payments, the User acknowledges that there is a valid lessor and lessee
+             relationship with the Beneficiary, and the transferred amount represents the actual rental value as per the agreement. PocketPG's role is solely to facilitate these 
+             rental payments from the User to the Beneficiary and does not involve itself in the underlying transactions between the User and the Beneficiary. The User is responsible 
+             for complying with any applicable regulations regarding such payments and will be held accountable for any non-compliance or defaults. PocketPG is not a financial 
+             institution and does not provide banking or related financial services. To provide the services and process transactions, PocketPG may utilize the services of one or 
+             more third parties. PocketPG is not an agent of the Lessor, the Lessee, or any other party involved in the remittance process.
+            </p>
+        </div>
+
+        <div>
+            <h2 className={styles.heading3}>KYC (Know Your Customer):</h2>
+            <p className={styles.p1}>
+             While the User is solely responsible for verifying the transaction's authenticity, PocketPG may request necessary documents or details from the User and the
+             Beneficiary to substantiate the lessor and lessee relationship, such as the Rental Agreement, PAN Card, Aadhar Card, etc. If the remittance amount exceeds INR 50,000, 
+             the User will be required to provide PAN card details or any other document/information as required by law. During Aadhaar-based verification, the User authorizes 
+             PocketPG to download the Aadhaar XML from the UIDAI website to verify KYC details.
+            </p>
+        </div>
+        <div>
+        <h2 className={styles.heading3}>Authorization:</h2>
+            <p className={styles.p1}>
+            By initiating a transaction on the platform, the User authorizes PocketPG to hold, receive, and disburse funds according to the payment instructions to the designated
+            bank account of the Beneficiary. This authorization allows PocketPG to charge the specified amount to the User's bank account, credit card, UPI account, debit card,
+            payment wallets, or other enabled payment instruments on the platform for remitting the amount to the designated bank account of the Beneficiary. When the User instructs 
+            PocketPG to pay the Beneficiary, they authorize and direct PocketPGl to allocate the payment (after deducting applicable fees, service charges, or other amounts specified 
+            in the agreement) to the designated person. In case the transaction cannot be processed due to incomplete details provided by the User, PocketPG will refund the amount after
+            deducting relevant service charges, transaction fees, and penalties.
+            </p>
+        </div>
+        
+        <div>
+        <h2 className={styles.heading3}>Transaction Terms:</h2>
+            <p className={styles.p1}>
+            The User is responsible for providing accurate bank account details of the Beneficiary during the transaction, and under no circumstances shall  PocketPG 
+            be held accountable for any incorrect credit due to inaccurate bank details. The User is also responsible for any fraudulent transactions that occur through their bank account, credit card, 
+            or any other payment method due to their negligence or sharing of financial details with third parties. PocketPG cannot be held responsible for such fraudulent transactions, and no refund 
+            will be issued once the money is successfully credited to the designated bank account of the Beneficiary. PocketPG reserves the right to delay, suspend, or reject a transaction if there 
+            are financial or security risks, suspicion of unauthorized or fraudulent activity, violation of the terms of the agreement, disputes, or any other unusual circumstances.
+            </p>
+        </div>
+
+        <div>
+        <h2 className={styles.heading3}>Liability:</h2>
+            <p className={styles.p1}>
+            The User agrees not to hold PocketPG liable for any disputes or issues arising between the User, the recipient, or any financial institution. The User agrees to indemnify
+            and hold PocketPG, its affiliates, and employees harmless against any claims or actions by any person or regulatory authorities related to transactions conducted with incorrect
+            details or credited to the wrong account.
+            </p>
+        </div>
+
+        <div>
+        <h2 className={styles.heading3}>Suspicious/Fraudulent Transactions:</h2>
+            <p className={styles.p1}>
+            If the User observes any suspicious or fraudulent transactions, they are required to notify PocketPG immediately. It is the User's responsibility to maintain the 
+            confidentiality of their financial details and not share them with others. PocketPG will take necessary measures to address reported suspicious/fraudulent transactions 
+            but will not be liable for any refunds or liabilities to any person or authority if the money has been successfully credited to the designated bank account of the Beneficiary
+            as provided by the User.
+            </p>
+        </div>
+
+        <div>
+        <h2 className={styles.heading3}>Refunds:</h2>
+            <p className={styles.p1}>
+            If PocketPG is unable to transfer the rental amount to the designated bank account of the Beneficiary due to technical issues, a refund will be issued to the same 
+            account from which the remittance was initiated. However, once the amount has been successfully credited to the Beneficiary's designated bank account, FindMyHostel will
+            not be liable to repay or refund the amount to the User. In case of chargebacks, if a transaction is disputed, reversed, unauthorized, or allegedly unlawful, suspicious, 
+            or in violation of the agreement, the Beneficiary will be responsible for reimbursing PocketPG for any associated chargebacks, fees, fines, or penalties. The User/Beneficiary 
+            is expected to cooperate in the investigation of disputed transactions, and failure to do so may result in an irreversible chargeback for which PocketPG shall not be held liable.    
+            </p>
+        </div>
+
+        </div>
+
+        
     )
 }
