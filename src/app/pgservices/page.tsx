49026849cca44d1f92a1d0d9dc32f223752ccca1
FernoLinux <fernooxplozd@gmail.com>
2024-03-16 13:15:23 +0530

    Edited Services by Ferno

--- src/app/pgservices/page.tsx
+++ src/app/pgservices/page.tsx
@@ -9,8 +9,36 @@ import Image from "next/image"
 export default function Pgservices()
 {
     return(
-        <div>
-        <h1>Hello Service</h1>
+        <div className={styles.main}>
+        <h3 className={styles.head1}>Why are we the best?</h3>
+        <h1 className={styles.head2}>Our Services</h1>
+        <div className={styles.container}>
+            <div className={styles.servicecard}>
+                <Image className={styles.pic} src={book} alt="hello"/>
+                <p className={styles.service}>Hostel Booking and Exploring the details</p>
+            </div>
+            <div className={styles.servicecard}>
+                <Image className={styles.pic} src={search} alt="image"/>
+                <p className={styles.service}>Location based Hostel Searching</p>
+            </div>
+            <div className={styles.servicecard}>
+                <Image className={styles.pic} src={add} alt="image"/>
+                <p className={styles.service}>Adding New Hostels</p>
+            </div>
+            <div className={styles.servicecard} >
+                <Image className={styles.pic} src={rating} alt="image"/>
+                <p className={styles.service}>Hostel Review based Comparison</p>
+            </div>
+            <div className={styles.servicecard}>
+                <Image className={styles.pic} src={filter} alt="image"/>
+                <p className={styles.service}>Filtering and Sorting the Hostels</p>
+            </div>
+            <div className={styles.servicecard}>
+                <Image className={styles.pic} src={choose} alt="image"/>
+                <p className={styles.service}>Choose services what Hostel provides.</p>
+            </div>
+        </div>
+        
         </div>
     )
 }
